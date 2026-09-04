c45e87344a348d0a208c941fff5e5152b1a7cc3d
Mavin <niduazaron@gmail.com>
2026-09-04 19:20:33 +0800

    Add files via upload

---
+++ Project Tetris/src/main/KeyHandler.java
@@ -0,0 +1,44 @@
+package main;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public static boolean upPressed, downPressed, leftPressed, rightPressed, pausePressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){ //UP Movement
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_A){ //LEFT Movement
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_S){ //DOWN Movement
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_D){ //RIGHT Movement
+            rightPressed = true;
+        }
+        if(code == KeyEvent.VK_SPACE){
+            if(pausePressed){
+                pausePressed = false;
+                GamePanel.music.play(0, true);
+                GamePanel.music.loop();
+            }
+            else {
+                pausePressed = true;
+                GamePanel.music.stop();
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
